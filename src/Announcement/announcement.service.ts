4cd21f63e0c702c2390d20c71669cee99bb2a4de
Laís Martins <silveiramartinslais@gmail.com>
2023-02-24 10:59:58 -0300

    feat: Announcement get/:id patch and delete services created

--- src/Announcement/announcement.service.ts
+++ src/Announcement/announcement.service.ts
@@ -1,16 +1,35 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { IRegisterAnnouncement } from "./dto/create.announcement.dto";
+import { IUpdateAnnouncement } from "./dto/update.announcement.dto";
 
 @Injectable()
 export class AnnouncementService {
-    constructor(private prisma: PrismaService) { }
+  constructor(private prisma: PrismaService) {}
 
-    findAll() {
-        return this.prisma.announcement.findMany({});
-    }
+  findAll() {
+    return this.prisma.announcement.findMany({});
+  }
 
-    create(registerAnnouncementData: IRegisterAnnouncement) {
-        return this.prisma.announcement.create({ data: registerAnnouncementData })
-    }
+  findOne(id: string) {
+    return this.prisma.announcement.findUnique({
+      where: { id: id },
+      include: { user: true },
+    });
+  }
+
+  create(registerAnnouncementData: IRegisterAnnouncement) {
+    return this.prisma.announcement.create({ data: registerAnnouncementData });
+  }
+
+  update(id: string, updateAnnouncement: IUpdateAnnouncement) {
+    return this.prisma.announcement.update({
+      where: { id: id },
+      data: updateAnnouncement,
+    });
+  }
+
+  remove(id: string) {
+    return this.prisma.announcement.delete({ where: { id: id } });
+  }
 }

f84f0ab7acb1124857fda7754824a77518cefff2
Laís Martins <silveiramartinslais@gmail.com>
2023-02-27 12:34:34 -0300

    feat: user interface updated

--- src/User/dto/create.user.dto.ts
+++ src/User/dto/create.user.dto.ts
@@ -1,12 +1,18 @@
 // import { ApiProperty } from "nestjs/swagger";
 
 export class IRegisterUser {
-  // @ApiProperty()
   name: string;
-
-  // @ApiProperty()
   email: string;
-
-  // @ApiProperty()
   password: string;
+  userImage?: string;
+  cpf: string;
+  contact: string;
+  birthDate?: string;
+  description?: string;
+  userType: "buyer" | "advertiser";
+  cep: string;
+  state: string;
+  city: string;
+  street: string;
+  complement?: string;
 }
